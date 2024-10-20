5b013e5f60f5e06f016b3e82ac93c9387d846f90
Jasperino64 <jasperschen@gmail.com>
2024-10-20 18:13:42 -0400

    fix types and seed

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -6,15 +6,20 @@ import { lusitana } from "@/app/ui/fonts"
 import { InvoicesTableSkeleton } from "@/app/ui/skeletons"
 import { Suspense } from "react"
 import { fetchInvoicesPages } from "@/app/lib/data"
-export default async function Page({
-  searchParams,
-}: {
-  searchParams?: { query?: string; page?: string }
-}) {
-  const query = searchParams?.query || ""
-  const currentPage = Number(searchParams?.page) || 1
 
-  const totalPages = await fetchInvoicesPages(query)
+type PageProps = {
+  searchParams?: Promise<{
+    query: string;
+    page: string;
+  }> | undefined;
+}
+
+export default async function Page({ searchParams }: PageProps) {
+  const params = await searchParams;
+  const query = params?.query || "";
+  const currentPage = Number(params?.page) || 1;
+
+  const totalPages = await fetchInvoicesPages(query);
   return (
     <div className="w-full">
       <div className="flex w-full items-center justify-between">
